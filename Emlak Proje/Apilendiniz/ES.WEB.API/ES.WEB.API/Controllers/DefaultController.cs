fae8ccf2e749bf303dc6b7cd7d99036b80e330b6
Bikace <34445727+Bikace@users.noreply.github.com>
2019-06-10 13:14:02 +0300

    Birkan
    Emlak Projesi

---
+++ Emlak Proje/Apilendiniz/ES.WEB.API/ES.WEB.API/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿
+using ES.Bussiness.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Mvc;
+
+namespace ES.WEB.API.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class DefaultController : ApiController
+    {
+        AdreManager ad = new AdreManager();
+        public List<string> Get()
+        {
+            var list = ad.GetAdrelar().Where(x => x.Il != null).Select(x => x.Il).Distinct().ToList();
+            return list;
+        }
+        public List<string> Get(string il)
+        {
+            var list = ad.GetAdrelar().Where(x => x.Il == il).Select(x => x.Ilce).Distinct().ToList();
+            return list;
+        }
+    }
+}
